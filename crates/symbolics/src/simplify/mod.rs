33111a4ede143be1c9a8d5788ae42b03e48ecf13
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-06-14 20:42:26 +0200

    Experimentation with a pattern matching engine.

--- crates/symbolics/src/simplify/mod.rs
+++ crates/symbolics/src/simplify/mod.rs
@@ -1,3 +1,4 @@
 pub mod normalize;
 mod naive;
 pub use naive::simplify_ast;
+pub mod pattern;
---
+++ crates/symbolics/src/simplify/pattern.rs
@@ -0,0 +1,107 @@
+use std::{collections::HashMap, ops};
+
+use numbers::RealScalar;
+
+use crate::parser::ast::AstNode;
+
+pub enum AstPattern<'a> {
+    Any(&'a str),
+    Number(&'a str),
+    Constant(RealScalar),
+    Add(Box<AstPattern<'a>>, Box<AstPattern<'a>>),
+}
+
+pub trait PatternRewrite
+where
+    Self: Sized,
+{
+    fn matches(&self, pattern: &AstPattern) -> Option<HashMap<String, Self>>;
+    fn rewrite<F>(&self, pattern: &AstPattern, apply_func: F) -> Self
+    where
+        F: Fn(&Self) -> Self;
+}
+
+impl ops::Add for AstPattern<'_> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        AstPattern::Add(Box::new(self), Box::new(rhs))
+    }
+}
+
+impl PatternRewrite for AstNode {
+    fn matches(&self, pattern: &AstPattern) -> Option<HashMap<String, Self>> {
+        let mut matches = HashMap::new();
+
+        match pattern {
+            AstPattern::Any(name) => {
+                matches.insert(name.to_string(), self.clone());
+            }
+            AstPattern::Number(name) => {
+                if let AstNode::Constant(_) = self {
+                    matches.insert(name.to_string(), self.clone());
+                } else {
+                    return None;
+                }
+            }
+            AstPattern::Constant(value) => {
+                if let AstNode::Constant(node_value) = self {
+                    if node_value != value {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            AstPattern::Add(left_pattern, right_pattern) => {
+                if let AstNode::Add(left, right) = self {
+                    let left_matches = left.matches(left_pattern);
+                    let right_matches = right.matches(right_pattern);
+
+                    if left_matches.is_none() || right_matches.is_none() {
+                        return None;
+                    }
+
+                    matches.extend(left_matches.unwrap());
+                    matches.extend(right_matches.unwrap());
+                } else {
+                    return None;
+                }
+            }
+        }
+
+        Some(matches)
+    }
+
+    fn rewrite<F>(&self, pattern: &AstPattern, apply_func: F) -> Self
+    where
+        F: Fn(&Self) -> Self,
+    {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parser::parse;
+
+    #[test]
+    fn test_pattern_matching() {
+        let ast = parse("2 * cos[1 + x] + 3").unwrap();
+
+        use AstPattern::*;
+        let pattern = Any("X") + Any("Y");
+
+        let matches = ast.matches(&pattern);
+        assert!(matches.is_some());
+
+        let matches = matches.unwrap();
+        assert!(matches.contains_key("X"));
+        assert!(matches.contains_key("Y"));
+
+        let x = matches.get("X").unwrap();
+        let y = matches.get("Y").unwrap();
+        assert_eq!(ast, AstNode::Add(Box::new(x.clone()), Box::new(y.clone())));
+    }
+}
