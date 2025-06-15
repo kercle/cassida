b847632a67987d507efa4ca8a3c92691e8374698
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2025-06-16 01:08:18 +0200

    Naive term simplification pattern matching engine.

--- crates/symbolics/src/simplify/mod.rs
+++ crates/symbolics/src/simplify/mod.rs
@@ -1,19 +1,98 @@
 // mod naive;
 pub mod normalize;
 
-use std::collections::HashMap;
-use std::vec;
+use std::collections::{HashMap, HashSet};
+use std::fmt::Debug;
+use std::hash::Hash;
+use std::{fmt, vec};
+
+use numbers::RealScalar;
+use numbers::integer::BigInteger;
 
 use crate::format::MathDisplay;
 use crate::parser::ast::AstNode;
 use crate::simplify::pattern::{AstPattern, BindingType, PatternRewriteOnceIter};
 pub mod pattern;
 
-pub fn simplify_ast(ast: AstNode) -> AstNode {
-    let initial_ast_repr_len = ast.to_yasc().len();
-    let mut equivalent_asts = vec![(ast.clone(), initial_ast_repr_len)];
-    let mut rewrite_tracker: HashMap<String, Vec<usize>> = HashMap::new();
+#[derive(Debug, Clone)]
+struct EquivalentAstEntry<PatternId> {
+    ast: AstNode,
+    metric: usize,
+    matched_patterns: HashSet<PatternId>,
+}
 
+#[derive(Clone)]
+struct EquivalentAst<PatternId>(HashMap<String, EquivalentAstEntry<PatternId>>);
+
+impl<PatternId: Eq + Hash + Debug> EquivalentAst<PatternId> {
+    fn new() -> Self {
+        EquivalentAst(HashMap::new())
+    }
+
+    fn insert(&mut self, ast: AstNode) {
+        let ast_repr = ast.to_yasc();
+        let metric = ast_repr.len();
+        self.0.insert(
+            ast_repr,
+            EquivalentAstEntry {
+                ast,
+                metric,
+                matched_patterns: HashSet::new(),
+            },
+        );
+    }
+
+    fn pattern_already_applied(&self, ast: &AstNode, pattern_id: &PatternId) -> bool {
+        let ast_repr = ast.to_yasc();
+        if let Some(entry) = self.0.get(&ast_repr) {
+            entry.matched_patterns.contains(pattern_id)
+        } else {
+            false
+        }
+    }
+
+    fn add_pattern(&mut self, ast: AstNode, pattern_id: PatternId) {
+        let ast_repr = ast.to_yasc();
+        if let Some(entry) = self.0.get_mut(&ast_repr) {
+            entry.matched_patterns.insert(pattern_id);
+        } else {
+            // If the AST is not found, we can insert it
+            self.insert(ast);
+            self.0
+                .get_mut(&ast_repr)
+                .unwrap()
+                .matched_patterns
+                .insert(pattern_id);
+        }
+    }
+
+    fn iter_asts(&self) -> impl Iterator<Item = &AstNode> {
+        self.0.iter().map(|(_, entry)| &entry.ast)
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn shortest_ast(&self) -> Option<&AstNode> {
+        self.0
+            .values()
+            .min_by_key(|entry| entry.metric)
+            .map(|entry| &entry.ast)
+    }
+}
+
+impl<T: Debug> fmt::Debug for EquivalentAst<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "EquivalentAst[")?;
+        for (repr, entry) in &self.0 {
+            write!(f, "{}:{:?}, ", repr, entry.matched_patterns)?;
+        }
+        write!(f, "]")
+    }
+}
+
+pub fn simplify_ast(ast: AstNode) -> AstNode {
     use AstPattern::*;
     let rules: Vec<(AstPattern, Box<dyn Fn(&BindingType) -> AstNode>)> = vec![
         (
@@ -24,16 +103,30 @@ pub fn simplify_ast(ast: AstNode) -> AstNode {
                 AstNode::add(b, a)
             }),
         ),
-        // (
-        //     Any("A") + Constant(RealScalar::zero()),
-        //     Box::new(|bind: &BindingType| bind.get("A").unwrap().clone()),
-        // ),
+        (
+            (Any("A") + Any("B")) + Any("C"),
+            Box::new(|bind: &BindingType| {
+                let a = bind.get("A").unwrap().clone();
+                let b = bind.get("B").unwrap().clone();
+                let c = bind.get("C").unwrap().clone();
+                AstNode::add(AstNode::add(a, c), b)
+            }),
+        ),
+        (
+            Any("A") + Any("A"),
+            Box::new(|bind: &BindingType| {
+                let a = bind.get("A").unwrap().clone();
+                AstNode::mul(
+                    AstNode::constant(RealScalar::Integer(BigInteger::from_u64(2))),
+                    a,
+                )
+            }),
+        ),
         (
             Number("A") + Number("B"),
             Box::new(|bind: &BindingType| {
                 let a = bind.get("A").unwrap().clone();
                 let b = bind.get("B").unwrap().clone();
-                dbg!("Pattern matches number: {}, {}", a.to_yasc(), b.to_yasc());
                 if let (
                     AstNode::Constant { value: a_val, .. },
                     AstNode::Constant { value: b_val, .. },
@@ -47,82 +140,34 @@ pub fn simplify_ast(ast: AstNode) -> AstNode {
         ),
     ];
 
+    let mut equivalent_asts = EquivalentAst::new();
+    equivalent_asts.insert(ast.clone());
+
     loop {
-        // TODO: Flatten nested loops for better readability
+        let equivalent_asts_len = equivalent_asts.len();
 
-        let num_equivalent_asts = equivalent_asts.len();
-
-        for (pattern_idx, (pattern, rewrite_rule)) in rules.iter().enumerate() {
-            let mut more_equivalent_asts = Vec::new();
-
-            for (ast, _) in equivalent_asts.iter() {
-                let ast_repr = ast.to_yasc();
-
-                println!("Applying pattern {} to {}", pattern_idx, ast_repr);
-                println!("Rewrite tracker: {:?}", rewrite_tracker);
-
-                if let Some(applied_patterns) = rewrite_tracker.get(&ast_repr) {
-                    println!(" -- Already applied patterns: {:?}", applied_patterns);
-                    if applied_patterns.contains(&pattern_idx) {
-                        continue; // Skip already applied patterns
-                    }
+        for ast in equivalent_asts.clone().iter_asts() {
+            for (pattern_id, (pattern, rewrite_rule)) in rules.iter().enumerate() {
+                if equivalent_asts.pattern_already_applied(ast, &pattern_id) {
+                    continue; // Skip if the pattern has already been applied to this AST
                 }
 
                 let mut rewrite_iter =
                     PatternRewriteOnceIter::new(ast.clone(), pattern, rewrite_rule);
 
                 while let Some(new_ast) = rewrite_iter.next() {
-                    println!(
-                        " -- Applying pattern {}: {} -> {}",
-                        pattern_idx,
-                        ast.to_yasc(),
-                        new_ast.to_yasc()
-                    );
-
-                    let new_ast_repr = new_ast.to_yasc();
-                    if rewrite_tracker.contains_key(&new_ast_repr) {
-                        continue; // Skip if the new AST is already in the list
-                    }
-
-                    more_equivalent_asts.push((new_ast, new_ast_repr.len()));
-                }
-
-                let ast_repr = ast.to_yasc();
-                if let Some(entry) = rewrite_tracker.get_mut(&ast_repr) {
-                    println!(
-                        "Adding pattern {} to existing entry for AST: {}",
-                        pattern_idx, ast_repr
-                    );
-                    entry.push(pattern_idx);
-                } else {
-                    rewrite_tracker.insert(ast_repr, vec![pattern_idx]);
+                    equivalent_asts.add_pattern(ast.clone(), pattern_id);
+                    equivalent_asts.insert(new_ast.clone());
                 }
             }
-
-            equivalent_asts.extend(more_equivalent_asts);
         }
 
-        if equivalent_asts.len() == num_equivalent_asts {
-            break; // No more rewrites possible
+        if equivalent_asts.len() == equivalent_asts_len {
+            break; // No new ASTs were generated, we can stop
         }
     }
 
-    let mut shortest_ast = ast;
-    let shortest_ast_len = initial_ast_repr_len;
-
-    for (equiv_ast, equiv_ast_len) in equivalent_asts {
-        println!(
-            "Equivalent AST (length: {}): {}",
-            equiv_ast_len,
-            equiv_ast.to_yasc(),
-        );
-        if equiv_ast_len < shortest_ast_len {
-            // Update the shortest AST if a shorter one is found
-            shortest_ast = equiv_ast;
-        }
-    }
-
-    shortest_ast
+    equivalent_asts.shortest_ast().cloned().unwrap()
 }
 
 #[cfg(test)]
@@ -132,7 +177,7 @@ mod tests {
 
     #[test]
     fn test_simplify_ast() {
-        let ast = parse("2+x+3").unwrap();
+        let ast = parse("x+x").unwrap();
         let simplified_ast = simplify_ast(ast);
 
         println!("Simplified AST: {}", simplified_ast.to_yasc());
