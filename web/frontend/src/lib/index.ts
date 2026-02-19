3d74b448c25d88ee6d52b2fd7e423d471098abb6
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-02-20 00:30:51 +0100

    Install daisyui and fontawesome in frontend.

--- web/frontend/src/lib/index.ts
+++ web/frontend/src/lib/index.ts
@@ -1,1 +1,59 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { writable } from 'svelte/store';
+
+type AppState = {
+    history: [any],
+};
+
+function createGlobalState() {
+    const { subscribe, set, update } = writable<{
+        data: AppState;
+        connected: boolean;
+    }>({
+        data: {} as AppState,
+        connected: false,
+    });
+
+    let socket: WebSocket;
+
+    function connect() {
+        const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+        const url = `${protocol}://${location.host}/ws`;
+        socket = new WebSocket(url);
+
+        socket.onopen = () => {
+            update(s => ({ ...s, connected: true }));
+        };
+
+        socket.onmessage = (event) => {
+            try {
+                const msg = JSON.parse(event.data);
+
+                update(s => {
+                    s.data.history.push(msg);
+                    return {
+                        ...s,
+                        connected: true,
+                    };
+                });
+            } catch (e) {
+                console.log("Received non-JSON or echo message:", event.data);
+            }
+        };
+
+        socket.onclose = () => {
+            update(s => ({ ...s, connected: false }));
+            setTimeout(connect, 3000);
+        };
+    }
+
+    if (typeof window !== 'undefined') {
+        connect();
+    }
+
+    return {
+        subscribe,
+        send: (msg: string) => socket?.send(msg)
+    };
+}
+
+export const appState = createGlobalState();
