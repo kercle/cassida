fcefd3105f024200f1ad0dc06fc61548ba14fb8c
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-02-20 01:02:16 +0100

    Sketch out result cells and input cells for web frontend.

--- web/frontend/src/routes/+page.svelte
+++ web/frontend/src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import InputCell from "./InputCell.svelte";
+	import ResultCell from "./ResultCell.svelte";
 
-<p>$a+4$</p>
+
+</script>
+<div class="p-8 flex flex-col gap-4">
+    <ResultCell />
+    <InputCell />
+</div>
