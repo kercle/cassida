ad40647fa03dd19bd7d5077fcb5ad791d500150c
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-03-19 23:59:17 +0100

    Migrate environment to ExprHandle.

--- crates/symbolics/src/pattern/environment.rs
+++ crates/symbolics/src/pattern/environment.rs
@@ -1,26 +1,26 @@
-use std::{collections::HashMap, fmt::Debug, rc::Rc};
+use std::{collections::HashMap, rc::Rc};
 
 use crate::{
     atom::Atom,
-    expr::{Expr, ExprKind, NormExpr, RawExpr},
+    expr::pool::{ExprHandle, ExprPool, ExprView, NormExprHandle, RawExprHandle},
     pattern::program::{Program, VarId},
 };
 
 #[derive(Clone)]
-pub(super) enum EnvBinding<'s> {
-    One(&'s NormExpr),
-    Many(Rc<Vec<&'s NormExpr>>),
+pub(super) enum EnvBinding {
+    One(NormExprHandle),
+    Many(Rc<Vec<NormExprHandle>>),
 }
 
-#[derive(Debug, Clone)]
-pub struct Environment<'p, 's> {
-    bindings: HashMap<VarId, EnvBinding<'s>>,
+#[derive(Clone)]
+pub struct Environment<'p> {
+    bindings: HashMap<VarId, EnvBinding>,
     program: &'p Program,
 }
 
 pub struct ErrorBindCollision;
 
-impl<'p, 's> Environment<'p, 's> {
+impl<'p> Environment<'p> {
     pub(super) fn new(program: &'p Program) -> Self {
         Self {
             bindings: HashMap::new(),
@@ -31,7 +31,7 @@ impl<'p, 's> Environment<'p, 's> {
     pub(super) fn bind_one(
         &mut self,
         bind_var: VarId,
-        subject: &'s NormExpr,
+        subject: NormExprHandle,
     ) -> Result<bool, ErrorBindCollision> {
         match self.bindings.get(&bind_var) {
             Some(EnvBinding::One(bound_subject)) => {
@@ -52,7 +52,7 @@ impl<'p, 's> Environment<'p, 's> {
     pub(super) fn bind_seq(
         &mut self,
         bind_var: VarId,
-        subjects: Rc<Vec<&'s NormExpr>>,
+        subjects: Rc<Vec<NormExprHandle>>,
     ) -> Result<bool, ErrorBindCollision> {
         match self.bindings.get(&bind_var) {
             Some(EnvBinding::Many(bound_subjects)) => {
@@ -87,18 +87,18 @@ impl<'p, 's> Environment<'p, 's> {
         self.program.var_ids.get(name.as_ref()).cloned()
     }
 
-    pub fn get_one<T: AsRef<str>>(&self, name: T) -> Option<&'s NormExpr> {
+    pub fn get_one<T: AsRef<str>>(&self, name: T) -> Option<NormExprHandle> {
         use EnvBinding::*;
 
         let var_id = self.var_id_from_name(name.as_ref())?;
 
         match self.bindings.get(&var_id)? {
-            One(val) => Some(val),
+            One(val) => Some(*val),
             Many(_) => None,
         }
     }
 
-    pub fn get_seq<T: AsRef<str>>(&self, name: T) -> Option<&[&'s NormExpr]> {
+    pub fn get_seq<T: AsRef<str>>(&self, name: T) -> Option<&[NormExprHandle]> {
         use EnvBinding::*;
 
         let var_id = self.var_id_from_name(name.as_ref())?;
@@ -110,54 +110,44 @@ impl<'p, 's> Environment<'p, 's> {
     }
 }
 
-impl<'p, 's> Environment<'p, 's> {
-    pub fn fill<S>(&self, target_expr: Expr<S>) -> RawExpr {
-        match target_expr.into_kind() {
-            ExprKind::Atom {
-                entry: Atom::Symbol(name),
-            } => {
+impl<'p> Environment<'p> {
+    pub fn fill<S: Copy + 'static>(
+        &self,
+        pool: &mut ExprPool,
+        target_expr: ExprHandle<S>,
+    ) -> RawExprHandle {
+        match target_expr.view(pool) {
+            ExprView::Atom(Atom::Symbol(name)) => {
                 // In case of a symbol -> Replace with blanks
                 self.get_one(&name)
-                    .cloned()
-                    .map(NormExpr::into_raw)
-                    .unwrap_or(RawExpr::new_symbol(name).into_raw())
+                    .map(|e| e.as_raw())
+                    .unwrap_or(target_expr.as_raw())
             }
-            ExprKind::Node { head, args } => {
-                let new_head = self.fill(*head);
+            ExprView::Node { head, args } => {
+                let new_head = self.fill(pool, head);
                 let mut new_args = vec![];
 
-                for arg in args.into_iter() {
-                    let Some(name) = arg.get_symbol() else {
+                let args_collected: Vec<ExprHandle<S>> = args.iter(pool).collect();
+                for arg in args_collected {
+                    let arg_view = arg.view(pool);
+                    let Some(name) = arg_view.get_symbol() else {
                         // Arg is not a symbol -> decend and push to new args
-                        new_args.push(self.fill(arg));
+                        new_args.push(self.fill(pool, arg));
                         continue;
                     };
 
                     if let Some(repl) = self.get_one(name) {
-                        new_args.push(repl.clone().into_raw());
+                        new_args.push(repl.as_raw());
                     } else if let Some(repl) = self.get_seq(name) {
-                        new_args.extend(repl.iter().map(|&e| e.clone().into_raw()));
+                        new_args.extend(repl.iter().map(|&e| e.as_raw()));
                     } else {
-                        new_args.push(arg.into_raw());
+                        new_args.push(arg.as_raw());
                     }
                 }
 
-                RawExpr::new_node(new_head, new_args)
+                pool.variadic_node(new_head, new_args)
             }
-            other => other.into_raw_expr(),
-        }
-    }
-}
-
-impl<'p, 's> Environment<'p, 's> {
-    pub fn dbg_print_bindings(&self) {
-        let mut keys: Vec<&VarId> = self.bindings.keys().collect();
-        keys.sort();
-
-        for k in keys {
-            let v = self.bindings.get(k).unwrap();
-            let name = self.program.vars.get(*k as usize).unwrap();
-            eprintln!("{name}: {v:?}");
+            _ => target_expr.as_raw(),
         }
     }
 }
